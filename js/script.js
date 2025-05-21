a669e8cfb87ade2143c3b07c088f96ad5ac315b6
Syariful Amar <kingnazaer221@gmail.com>
2025-05-21 10:17:59 +0700

    menambahkan mode dark

--- js/script.js
+++ js/script.js
@@ -20,3 +20,21 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+// dark mode
+let theme = "light";
+function toggleDarkTheme() {
+  if (sessionStorage.getItem("theme") === "light") {
+    sessionStorage.setItem("theme", "dark");
+    document.documentElement.classList.add("dark");
+  } else {
+    sessionStorage.setItem("theme", "light");
+    document.documentElement.classList.remove("dark");
+  }
+}
+document
+  .querySelector(".toggle-circle")
+  .addEventListener("click", toggleDarkTheme);
+if (sessionStorage.getItem("theme") === "dark") {
+  document.documentElement.classList.add("dark");
+}
